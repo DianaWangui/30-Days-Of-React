9f8cbcffc598f07e1955687f2fe45ed779c91d5f
DianaWangui <dianawanguikt048@gmail.com>
2024-06-16 18:41:50 +0300

    first commit

---
+++ my-react-app/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const myFirstElement = <h1>Hello Diana welcome to react</h1>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(myFirstElement);
